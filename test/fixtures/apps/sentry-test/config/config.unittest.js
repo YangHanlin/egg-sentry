1e21777f53a0deee0f3a3466a74102868938b25f
Yang Hanlin <mattoncis@hotmail.com>
2021-03-25 18:16:33 +0800

    test: update test to use Sentry DSN from environment variable

--- test/fixtures/apps/sentry-test/config/config.unittest.js
+++ test/fixtures/apps/sentry-test/config/config.unittest.js
@@ -1,7 +1,11 @@
 'use strict';
 
+require('dotenv').config({
+  path: __dirname + '/../.env',
+});
+
 exports.keys = '123456';
 
 exports.sentry = {
-  dsn: 'https://819e74a6e948468b9740680cfa87986b:38aaa0c0d51e463597493c250ff11f83@sentry.io/none_exist',
+  dsn: process.env.TEST_SENTRY_DSN,
 };
